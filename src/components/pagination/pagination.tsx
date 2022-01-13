ab875c0be3c79bb9d020d27dac8b219aba798735
Viktor Zdorovcev <v.zdvacew@gmail.com>
2022-01-13 19:00:48 +0700

    fix: Исправлена ошибка при переключении страниц

--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -24,9 +24,7 @@ function Pagination({ totalGuitars = 0 }: PaginationProps) {
   const [pageGroup, setPageGroup] = React.useState<number>(DEFAULT_SELECTED_PAGE_GROUP);
 
   React.useEffect(() => {
-    if (currentPage > DEFAULT_SELECTED_PAGE) {
-      updateSearchParams(ApiSearchParamKey.Page, currentPage);
-    }
+    updateSearchParams(ApiSearchParamKey.Page, currentPage);
   }, [currentPage]);
 
   React.useEffect(() => {
