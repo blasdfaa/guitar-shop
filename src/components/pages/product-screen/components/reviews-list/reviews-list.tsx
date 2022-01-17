de478ba441d561ba23cfa5bcaaa1a67c9f79ea5b
Viktor Zdorovcev <v.zdvacew@gmail.com>
2022-01-18 00:29:35 +0700

    feat: Добавлена дозагрузка отзывов по скроллу

--- src/components/pages/product-screen/components/reviews-list/reviews-list.tsx
+++ src/components/pages/product-screen/components/reviews-list/reviews-list.tsx
@@ -16,6 +16,35 @@ const DISPLAYED_REVIEWS_COUNT_STEP = 3;
 function ReviewsList({ reviews = [], onClickSendReview }: ReviewsListProps) {
   const [reviewsToShow, setReviewsToShow] = React.useState(DEFAULT_DISPLAYED_REVIEWS_COUNT);
 
+  const showMoreBtnRef = React.useRef(null);
+
+  React.useEffect(() => {
+    document.addEventListener('scroll', handleScroll);
+
+    return () => {
+      document.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const handleScroll = () => {
+    if (isElementVisible(showMoreBtnRef)) {
+      handleShowMoreReviews();
+    }
+  };
+
+  const isElementVisible = (ref: React.MutableRefObject<HTMLElement | null>): boolean | void => {
+    if (ref.current) {
+      const element = ref.current;
+      const coords = element.getBoundingClientRect();
+      const windowHeight = document.documentElement.clientHeight;
+
+      const topVisible = coords.top > 0 && coords.top < windowHeight;
+      const bottomVisible = coords.bottom < windowHeight && coords.bottom > 0;
+
+      return topVisible || bottomVisible;
+    }
+  };
+
   const handleShowMoreReviews = (): void => {
     setReviewsToShow((prevCount) => prevCount + DISPLAYED_REVIEWS_COUNT_STEP);
   };
@@ -38,7 +67,11 @@ function ReviewsList({ reviews = [], onClickSendReview }: ReviewsListProps) {
           .slice(0, reviewsToShow)
           .map((comments) => <ProductReviewItem key={comments.id} {...comments} />)}
       {!isAllReviewsShown && (
-        <button className="button button--medium reviews__more-button" onClick={handleShowMoreReviews}>
+        <button
+          className="button button--medium reviews__more-button"
+          onClick={handleShowMoreReviews}
+          ref={showMoreBtnRef}
+        >
           Показать еще отзывы
         </button>
       )}
